c0bbb8c1a594d30fa83c1421cfbe90f81aa2c5ad
Joao Felipe <jofelipe2018@gmail.com>
2025-04-22 20:15:08 -0300

    capturando valores dos inputs da lista de tarefas para a function adicionar()

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // Função para adicionar um gasto à tabela
 function adicionar() {
-
+  // Captura os valores dos inputs
+  const descricao = document.getElementById('ipt').value;
+  const valor = document.getElementById('ipt').value;
+  const categoria = document.getElementById('categorias').value;
 }
